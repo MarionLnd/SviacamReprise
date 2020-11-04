65f4a243b07fc8449238607df13df6c0801b5944
marieme <ps.marieme95@gmail.com>
2020-11-04 20:39:23 +0100

    Deuxieme Essai Hand Gesture

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,4 @@
 // Set width and height of the stage and webcam
-//var fp = require('fingerpose');
-
 let width = window.innerWidth - 300
 let height = window.innerHeight - 100
 
@@ -32,17 +30,87 @@ let webcam = new Konva.Image({
 });
 layer.add(webcam);
 
-
-/*
-
-const model = await handpose.load();
-const predictions = await model.estimateHands(webcam, true);
-
-// using a minimum confidence of 7.5 (out of 10)
-const estimatedGestures = GE.estimate(predictions.landmarks, 7.5);
-*/
-
 let imageObj = new Image(width, height)
+
+const landmarkColors = {
+    thumb: 'red',
+    indexFinger: 'blue',
+    middleFinger: 'yellow',
+    ringFinger: 'green',
+    pinky: 'pink',
+    palmBase: 'white'
+};
+
+const gestureStrings = {
+    'thumbs_up': '👍',
+    'victory': '✌🏻'
+};
+
+async function main() {
+    const video = document.querySelector("#pose-video");
+    const canvas = document.querySelector("#pose-canvas");
+    const ctx = canvas.getContext("2d");
+    const resultLayer = document.querySelector("#pose-result");
+
+    const knownGestures = [
+        fp.Gestures.VictoryGesture,
+        fp.Gestures.ThumbsUpGesture
+    ];
+    const GE = new fp.GestureEstimator(knownGestures);
+
+    // load handpose model
+    const model = await handpose.load();
+    console.log("Handpose model loaded");
+
+    const estimateHands = async () => {
+
+        // clear canvas overlay
+        ctx.clearRect(0, 0, video.width, video.height);
+        resultLayer.innerText = '';
+
+        // get hand landmarks from video
+        // Note: Handpose currently only detects one hand at a time
+        // Therefore the maximum number of predictions is 1
+        const predictions = await model.estimateHands(video, true);
+
+        for(let i = 0; i < predictions.length; i++) {
+
+            // draw colored dots at each predicted joint position
+            for(let part in predictions[i].annotations) {
+                for(let point of predictions[i].annotations[part]) {
+                    drawPoint(ctx, point[0], point[1], 3, landmarkColors[part]);
+                }
+            }
+
+            // now estimate gestures based on landmarks
+            // using a minimum confidence of 7.5 (out of 10)
+            const est = GE.estimate(predictions[i].landmarks, 7.5);
+
+            if(est.gestures.length > 0) {
+
+                // find gesture with highest confidence
+                let result = est.gestures.reduce((p, c) => {
+                    return (p.confidence > c.confidence) ? p : c;
+                });
+
+                resultLayer.innerText = gestureStrings[result.name];
+            }
+        }
+
+        // ...and so on
+        setTimeout(() => { estimateHands(); }, 1000 / video.fps);
+    };
+    estimateHands();
+
+    console.log("Starting predictions");
+}
+
+function drawPoint(ctx, x, y, r, color) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
 
 //Function to play the video
 function get_video() {
@@ -56,7 +124,13 @@ function get_video() {
                 console.log("Webcam is working")
                 video.srcObject = stream
                 video.play()
+                video.addEventListener("loadeddata", event => {
+                    console.log("Camera is ready");
+                    main();
+                });
+
                 core = core()
+                main()
             })
             .catch(function (error) {
                 console.log("Webcam is not working")
@@ -66,5 +140,3 @@ function get_video() {
 }
 
 get_video()
-
-
